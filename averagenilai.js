6d2bab11eb50bade6a0f596cdde776c02ffc366b
hendris27 <88545100+hendris27@users.noreply.github.com>
2023-03-24 09:26:29 +0700

    Add files via upload
    revisi

--- averagenilai.js
+++ averagenilai.js
@@ -1,23 +1,20 @@
-const a =70
-const b =90
-const c =60
-const d =30
-let hasil =(a+b+c+d)/4;
-console.log(a,"+",b,"+",c,"+",d,"=",hasil)
+const Math = 70
+const Sience = 90
+const English = 60
+const indonesian = 70
+let hasil = (Math + Sience + English + indonesian) / 4;
+console.log(Math, "+", Sience, "+", English, "+", indonesian, "=", hasil)
 
-if (hasil >= 90 ) {
-    console.log("A");
-  } else if (hasil >= 80 && hasil < 90) {
-    console.log("B");
-} else if(hasil >70 && hasil < 79 ) {
-    console.log("C");
-}else if(hasil >60 && hasil < 69 ) {
-    console.log("D");
-}
-  
-   else if(a == '' ||b == ''||c == ''||d == '' ) {
-    console.log("salah isi nilai tidak boleh kosong");
-}
-  else {
+if (hasil >= 90) {
+  console.log("A");
+} else if (hasil >= 80 && hasil < 90) {
+  console.log("B");
+} else if (hasil > 70 && hasil < 79) {
+  console.log("C");
+} else if (hasil > 60 && hasil < 69) {
+  console.log("D");
+} else if (Math == '' || Sience == '' || English == '' || indonesian == '') {
+  console.log("salah isi nilai tidak boleh kosong");
+} else {
   console.log("E");
 }
